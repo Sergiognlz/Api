6ee689e38e61d50f42956f3d22bbd20876de3daf
Sergiognlz <145675168+Sergiognlz@users.noreply.github.com>
2025-10-15 10:37:33 +0200

    api

---
+++ Tema_1/Proyectos/api_users/users.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+
+class User(BaseModel):
+    id:int
+    name:str
+    surname:str
+    age:int
+
+user_list= [
+        User(id=1, name="Sergio", surname="González",age=38),
+        User(id=2, name="Juan Luís", surname="Barrionuevo",age=21), 
+        User(id=3, name="Pablo", surname="Iglesias",age=46),
+        User(id=4, name="Perro", surname="Sánchez",age=53)  
+         
+]
+
+
+@app.get("/users")
+
+def users():
+
+    return user_list
+
+@app.get("/users/{id_user}")
+
+def get_user(id_user:int):
+    users=[user for user in user_list if user.id==id_user]
+
+ 
+    return users[0] if users else {"error":"User not foud"}
+
+
