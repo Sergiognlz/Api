2b5909139d3779b5694111e96f8b66a28aa7f8ac
Sergio <sergio.gonzalez@iesnervion.es>
2025-10-20 18:25:26 +0200

    1

--- Tema_1/ApiRest/Autores/Autores.py
+++ Tema_1/ApiRest/Autores/Autores.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+# Crear la aplicación de FastAPI
 app=FastAPI()
 
-
+# Definición del modelo Autor
 class Autor(BaseModel):
     id:int
     dni:str
     nombre:str
     apellidos:str
 
+# Lista de autores de ejemplo
 lista_autores= [
         Autor(id=1, dni="12345678A", nombre="H.P.", apellidos="Loveftcraft"),
         Autor(id=2, dni="12345678B", nombre="Frank Belknap", apellidos="Long"),
@@ -19,19 +21,21 @@ lista_autores= [
          
 ]
 
-
+# Endpoint para obtener la lista de autores
 @app.get("/autores")
-
 def Autores():
-
     return lista_autores
 
+# Endpoint para obtener un autor por su ID
 @app.get("/autores/{id_autor}")
-
 def get_autor(id_autor:int):
     autores=[autor for autor in lista_autores if autor.id==id_autor]
 
- 
     return autores[0] if autores else {"error":"Autor no encontrado"}
 
+# Autor por query 
+@app.get("/autor/")
+def get_autor_query(id_autor:int):
+    autores=[autor for autor in lista_autores if autor.id==id_autor]
 
+    return autores[0] if autores else {"error":"Autor no encontrado"}
--- Tema_1/Proyectos/Prueba1/Main.py
+++ Tema_1/Proyectos/Prueba1/Main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from Tema_1.ApiRest.Autores.Autores import app as autores_app
 
+# Crear la aplicación principal
 app=FastAPI()
 
-@app.get("/")
 
+@app.get("/")
+# Endpoint raíz
 def root():
-    return{"Hello":"World"}
+    return{"mensaje":"Bienvenido a la Api de Autores"}
+
+# Montar la aplicación de autores en el path /api
+app.mount("/api", autores_app)
