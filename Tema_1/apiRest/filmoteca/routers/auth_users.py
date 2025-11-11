4994dfccc28a2c4efdd5af70b011ede1bc1d8957
Sergiognlz <145675168+Sergiognlz@users.noreply.github.com>
2025-11-11 14:37:31 +0100

    1

--- Tema_1/apiRest/filmoteca/routers/auth_users.py
+++ Tema_1/apiRest/filmoteca/routers/auth_users.py
@@ -1,10 +1,11 @@
 
+import datetime
 from pydantic import BaseModel
 import jwt
 from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 
 # Configuración del router
 router = APIRouter()
@@ -69,16 +70,37 @@ users_db = {
 # Endpoint para el registro de nuevos usuarios
 @router.post("/register", status_code=201)
 def register(user: UserDB):
+    # Verificar si el usuario ya existe
     if user.username not in users_db:
-    
+        # Hashear la contraseña antes de almacenarla
         hashed_password = password_hash.hash(user.password)
+        # Almacenar el nuevo usuario en la "base de datos"
         user.password = hashed_password
+        # Agregar el usuario al diccionario de usuarios
         users_db[user.username] = user
-
+        # Devolver el usuario registrado (sin la contraseña)
         return user
 
     else:
         raise HTTPException(status_code=409, detail="El usuario ya existe")
 
    # Endpoint para el login de usuarios y generación de tokens JWT
-#@router.post("/login")
+@router.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    # Verificar si el usuario existe
+    username= users_db.get(form.username)
+
+    if username in users_db:
+        # Obtener el usuario de la "base de datos"
+        if password_hash.verify(form.password, users_db[form.username]["password"]):
+            # Crear el token JWT
+            expire= datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            access_token= {"sub":username, "exp": expire}
+            # Generar el token
+            token= jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
+            # Devolver el token
+            return {"access_token": token, "token_type": "bearer"}
+        
+
+    # Si el usuario no existe o la contraseña es incorrecta    
+    raise HTTPException(status_code=401, detail="El usuario o la contraseña son incorrectos")
